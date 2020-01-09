d96e59abe160a31c65b8977db0abb47b8646a40a
Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
2020-01-09 00:51:39 -0500

    Add init and emulationCycle declaration

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -8,14 +8,16 @@ class chip8 {
         unsigned char memory[4096];
         unsigned char V[16]; // Register
         unsigned short index;
-        unsigned short program_counter;
+        unsigned short pc;
         unsigned char gfx[64*32];
 
         unsigned char delay_timer;
         unsigned char sound_timer;
 
         unsigned short stack[16];
-        unsigned short sp;
+        unsigned short stack_pointer;
 
         unsigned char key[16];
+        void init();
+        void emulateCycle();
 };
